a1f27f7b9d66fb148a06cc8c7b80c3ff44a5a3f4
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-09-23 09:37:27 +0300

    Change dir before running py script

--- test/e2e/v1beta1/scripts/gh-actions/run-e2e-tests-papermill.sh
+++ test/e2e/v1beta1/scripts/gh-actions/run-e2e-tests-papermill.sh
@@ -11,6 +11,11 @@ ls -l
 # Split the string into an array
 IFS=',' read -r -a EXP_ARRAY <<< "$EXPERIMENTS"
 
+cd "../../test/e2e/v1beta1/scripts/gh-actions/" || exit 1
+echo "Changed to tests directory: $(pwd)"
+echo "Directory contents:"
+ls -l
+
 # Loop through each experiment and run the Python script
 for EXP in "${EXP_ARRAY[@]}"; do
   echo "Running experiment: $EXP"
