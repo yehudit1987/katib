459c9d32ee8b4a3f1322422855095dc93c7aa15e
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-09-26 09:42:45 +0300

    Deploy cluster and Katib - remove gpu

--- test/e2e/v1beta1/scripts/gh-actions/setup-minikube.sh
+++ test/e2e/v1beta1/scripts/gh-actions/setup-minikube.sh
@@ -25,22 +25,13 @@ DEPLOY_KATIB_UI=${1:-false}
 TUNE_API=${2:-false}
 TRIAL_IMAGES=${3:-""}
 EXPERIMENTS=${4:-""}
-GPU_REQUIRED=${5:-false}
 
 function check_minikube() {
   if minikube status >/dev/null 2>&1; then
     echo "Minikube is already running."
   else
-    if [[ "$GPU_REQUIRED" == "true" ]]; then
-      echo "Minikube is not running. Starting Minikube with GPU support..."
-      # Start Minikube with GPU support
-      minikube start --driver=kvm2
-      # Enable the NVIDIA GPU device plugin
-      minikube addons enable nvidia-gpu-device-plugin
-    else
-      echo "Minikube is not running. Starting Minikube without GPU support..."
-      minikube start --driver=kvm2
-    fi
+    echo "Minikube is not running. Starting Minikube..."
+    minikube start
   fi
 }
 
