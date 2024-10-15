1712bbf3861016b9f1f5c54a00441487975c0040
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-10-15 13:56:02 +0300

    tune-train-from-func.ipynb - add job kind to create job

--- test/e2e/v1beta1/scripts/gh-actions/run-e2e-sdk-tests-papermill.py
+++ test/e2e/v1beta1/scripts/gh-actions/run-e2e-sdk-tests-papermill.py
@@ -1,28 +1,11 @@
 import argparse
-import fileinput
 import logging
 import sys
 
 import papermill as pm
 import os
-import shutil
 
-
-#TODO: Later to run as part of git job
-def prepare_file(source_path, destination_path):
-
-    if not os.path.exists(destination_path):
-        try:
-            shutil.copy(source_path, destination_path)
-            print(f"Copied file from {source_path} to {destination_path}")
-        except Exception as e:
-            print(f"Failed to copy file: {e}")
-    else:
-        print(f"File already exists at {destination_path}")
-
-
-
-def run_notebook(experiment_path, namespace, verbose):
+def run_notebook(experiment_path, verbose):
 
     logging.debug("In run notebook")
 
@@ -57,9 +40,6 @@ def main():
         help="Path to the Katib Experiment (Jupyter notebook).",
     )
     parser.add_argument(
-        "--namespace", type=str, required=False, help="Namespace for the Katib E2E test",
-    )
-    parser.add_argument(
         "--verbose", action="store_true", help="Verbose output for the Katib E2E test",
     )
     args = parser.parse_args()
@@ -78,30 +58,9 @@ def main():
     # Check available Jupyter kernels
     logging.info("Available Jupyter kernels:")
     os.system("jupyter kernelspec list")
-    #TODO: Later to be implement as part of git job
-    # source_path_api_pb2 = "pkg/apis/manager/v1beta1/python/api_pb2.py"
-    # destination_path_katib_api_pb2 = "sdk/python/v1beta1/kubeflow/katib/katib_api_pb2.py"
-    # source_path_api_pb2_grpc = "pkg/apis/manager/v1beta1/python/api_pb2_grpc.py"
-    # destination_path_api_pb2_grpc = "sdk/python/v1beta1/kubeflow/katib/katib_api_pb2_grpc.py"
-    # prepare_file(source_path_api_pb2, destination_path_katib_api_pb2)
-    # prepare_file(source_path_api_pb2_grpc, destination_path_api_pb2_grpc)
-    #
-    # def replace_imports(filepath, original_import, new_import):
-    #     with fileinput.FileInput(filepath, inplace=True, backup='.bak') as file:
-    #         for line in file:
-    #             print(line.replace(original_import, new_import), end='')
-    #
-    # # Path to the file that needs the import change
-    # file_to_modify = "sdk/python/v1beta1/kubeflow/katib/katib_api_pb2_grpc.py"
-    #
-    # # Replace the original import with the new one
-    # replace_imports(file_to_modify, 'import api_pb2 as api__pb2', 'from . import katib_api_pb2 as api__pb2')
 
     # Run the notebook and check its output
-    success = run_notebook(args.experiment_path, args.namespace, args.verbose)
-
-
-    logging.debug(f"arguments are: {args.experiment_path, args.namespace, args.verbose}")
+    success = run_notebook(args.experiment_path, args.verbose)
 
     logging.info("---------------------------------------------------------------")
     logging.info("---------------------------------------------------------------")
